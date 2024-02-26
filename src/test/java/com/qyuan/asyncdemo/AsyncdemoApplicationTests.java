5e5ea81e3a50ad2fd122611f490a871bdc30c680
qyuan <352304083@qq.com>
2024-02-26 15:18:34 +0800

    init

---
+++ src/test/java/com/qyuan/asyncdemo/AsyncdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qyuan.asyncdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
